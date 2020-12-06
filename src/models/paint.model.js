897c7ada46d42547bcb3c7bef20cc15788c0aa4a
DavidFFDAW <davidferflo2@gmail.com>
2020-12-06 22:59:41 +0100

    feat(paint.model): added the model to the project

---
+++ src/models/paint.model.js
@@ -0,0 +1,11 @@
+class ToolbarModel{
+    
+    constructor(){
+        this.isPencilSelected = false;
+        this.isFillSelected = false;
+        this.color = '#000';
+        this.lineThick = '3';
+        this.isDrawing = false;
+        this.positions = { x: 0, y: 0 };
+    }
+}
